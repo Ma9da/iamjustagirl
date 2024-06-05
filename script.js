82a000a9077685110f3fd117631aed5b18faeedf
ma9da <dodymasry25@gmail.com>
2024-06-06 00:15:39 +0300

    encansulating my code

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-$(document).ready(function() {
-    $("#addPostButton").click(function() {
-        var postContent = $("#postContent").val();
-        if (postContent) {
-            var newPost = `
-                <div class="post">
-                    <div class="post-author">
-                        <figure class="post-author-img">
-                            <img src="https://img.freepik.com/free-photo/anime-stylecelebrating-valentines-day_23-2151257994.jpg" alt="">
-                        </figure>
-                        <p class="post-author-name">
-                            john smith
-                        </p>
-                        <p class="post-author-date">
-                            ${new Date().toLocaleDateString()}
-                        </p>
-                    </div>
-                    <p class="post-content">
-                        ${postContent}
+function addPost(){
+    const postContent = $("#postContent").val();
+    if (postContent) {
+        const newPost = `
+            <div class="post">
+                <div class="post-author">
+                    <figure class="post-author-img">
+                        <img src="https://img.freepik.com/free-photo/anime-stylecelebrating-valentines-day_23-2151257994.jpg" alt="">
+                    </figure>
+                    <p class="post-author-name">
+                        john smith
                     </p>
-                </div>`;
-            $(".posts").append(newPost);
-            $("#postContent").val("");  
-        } else {
-            alert("Please write something in the post.");
-        }
-    });
+                    <p class="post-author-date">
+                        ${new Date().toLocaleDateString()}
+                    </p>
+                </div>
+                <p class="post-content">
+                    ${postContent}
+                </p>
+            </div>`;
+        $(".posts").append(newPost);
+        $("#postContent").val("") 
+    } else {
+        alert("Please write something in the post.");
+    }
+}
+$(document).ready(function() {
+    $("#addPostButton").click(addPost);
     $(".hide").click(function(){
         $(".profile-img").hide("slow")
     })
