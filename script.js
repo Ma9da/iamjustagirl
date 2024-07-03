9646df3186d57e54b851744925a891e47415b51a
ma9da <dodymasry25@gmail.com>
2024-07-03 20:49:36 +0300

    cleaning code

--- script.js
+++ script.js
@@ -33,33 +33,31 @@ function imagDisplay(){
         }
     });    
 }
-$(document).ready(() => {
-    $("#addPostButton").click(addPost);
-    $(".img-display").click(imagDisplay)
-    $(".border").click(function () {
+function addBorder(){
         $(".profile-img img").toggleClass("red-borders")
-    })
-    $("p").click(function(){
+}
+function changePcolor(){
         $(this).css("color", "red")
-    })
-    $(".breakfast-wrapper").hide()
-    $( ".breakfast-btn" ).click(function(){
-        $(".breakfast-wrapper").show()
+}
+function breakFast(){
+        $(".breakfast-wrapper").show();
+        
         $("#milk").css({
             "position": "relative",
         });
-        $("#milk").animate( {
+    
+        $("#milk").animate({
             bottom: 54,
             left: 20,
         }, {
-            duration: "slow",  // 
-        step: function() {
-            $(this).css({
-                'transform': 'rotate(45deg)'
-            });
-        },
+            duration: "slow",
+            step: function() {
+                $(this).css({
+                    'transform': 'rotate(45deg)'
+                });
+            },
             complete: function() {
-                
+                // This function is called once the animation on #milk is complete
                 $("#box").css({
                     'transform': 'rotate(' + 320 + 'deg)',
                     '-ms-transform': 'rotate(' + 320 + 'deg)', /* IE 9 */
@@ -68,10 +66,22 @@ $(document).ready(() => {
                     '-moz-transform': 'rotate(' + 320 + 'deg)' /* Firefox */
                 });
                 $( "#cereal img" ).attr("src", "./assets/images/pngtree-fruit-cereal-breakfast-icon-cartoon-vector-picture-image_7852498.png");
-                $( "#cereal img" ).animate({ right: '+=50' });
+                $( "#cereal img" ).animate({ right: '+=50' }, {
+                    duration: "slow",
+                    complete: function() {
+                        // This function is called once the animation on #cereal img is complete
+                        $(".breakfast-wrapper").hide("slow");
+                    }
+                });
             }
         });
-    });
-    
-    
+}
+
+$(document).ready(() => {
+    $("#addPostButton").click(addPost);
+    $(".img-display").click(imagDisplay)
+    $(".border").click(addBorder)
+    $("p").click(changePcolor)
+    $(".breakfast-wrapper").hide()
+    $( ".breakfast-btn" ).click(breakFast)
 }); 
